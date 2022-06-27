0cf3445a5161c180143bb959b91624c6dc17154f
adhig93 <81528871+adhig93@users.noreply.github.com>
2022-06-27 08:47:55 +0530

    Update WorksWithHerokuServlet.java

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("WEB APP WORKING");
+        writer.print("WEB APP WORKING - Version 2");
         writer.close();
     }
 }
